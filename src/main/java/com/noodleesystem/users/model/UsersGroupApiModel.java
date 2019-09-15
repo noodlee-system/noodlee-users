4ca7f3394324a00d1e7605e67aa8ec7411fc192d
Robert Suszek <robert.suszek96@gmail.com>
2019-09-15 14:04:29 +0200

    UsersGroupApiModel creation.

---
+++ src/main/java/com/noodleesystem/users/model/UsersGroupApiModel.java
@@ -0,0 +1,52 @@
+package com.noodleesystem.users.model;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "groups")
+public class UsersGroupApiModel {
+
+    private int id;
+    private String name;
+    private Date startDate;
+    private Date endDate;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name", nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "start_date", nullable = false)
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    @Column(name = "end_date", nullable = false)
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+}
