c0e2a799583ab962297715ecadc96a1e17a6b8ea
Robert Suszek <robert.suszek96@gmail.com>
2019-09-15 15:17:24 +0200

    Added many to many between Teacher and Course

--- src/main/java/com/noodleesystem/users/model/TeacherApiModel.java
+++ src/main/java/com/noodleesystem/users/model/TeacherApiModel.java
@@ -1,11 +1,28 @@
 package com.noodleesystem.users.model;
 
 import javax.persistence.*;
+import java.util.HashSet;
 
 @Entity
 @Table(name = "teachers")
 @PrimaryKeyJoinColumn(name = "user")
 public class TeacherApiModel extends UserApiModel {
     public TeacherApiModel() {
+    }
+
+    @ManyToMany(cascade = { CascadeType.ALL })
+    @JoinTable(
+            name = "courses",
+            joinColumns = { @JoinColumn(name = "id") },
+            inverseJoinColumns = { @JoinColumn(name = "id") }
+    )
+    Set<CourseApiModel> courses = new HashSet<CourseApiModel>();
+
+    public Set<CourseApiModel> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Set<CourseApiModel> courses) {
+        this.courses = courses;
     }
 }
